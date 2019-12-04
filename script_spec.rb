52cc32d9da4efcc9207772305609329313d80490
mkalatski <maksim.kalatski@corevist.comgit config --global user.email maksim.kalatski@corevist.comgit config --global user.email maksim.kalatski@corevist.com>
2019-12-04 15:10:42 +0300

    FirstAuto

---
+++ script_spec.rb
@@ -0,0 +1,27 @@
+require 'capybara/rspec'
+require 'webdrivers'
+require 'capybara-screenshot/rspec'
+
+feature 'example' do
+
+  Capybara.app_host = 'https://google.com'
+  Capybara.run_server = false
+  Capybara.default_driver = :selenium_chrome
+
+  scenario 'sitelogin' do
+    visit 'https://forum.awd.ru'
+    find_all(:link, 'Вход').first.click
+    fill_in 'username', with: 'silent_stn'
+    fill_in 'password', with: 'asdfcvbn'
+    check 'autologin'
+    check 'viewonline'
+    click_button 'Вход'
+    expect(page).to have_content ('Информация')
+    click_on 'Выход'
+    expect(page).to have_content ('Вы успешно вышли из системы')
+    screenshot_and_open_image
+  end
+end
+
+
+
